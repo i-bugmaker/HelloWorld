533e562ace4ed254323b36da9e55d73f106b43ef
mh <291147521@qq.com>
2022-04-20 21:34:17 +0800

    add a menu

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 
@@ -13,5 +15,18 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "add a item!", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "remove a item!", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
